17b9b2c635c80478b4ad353c57aafa5577a9b047
sesamestrong <doolie.healey@gmail.com>
2020-01-19 18:21:37 -0500

    Fix sample sunday error

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ const getUrl = (title, undTitle, year, month, day, res) => {
     const isSunday = theDate.getDay() === 0;
     return "https://safr.kingfeatures.com/api/img.php?e=gif&s=c&file=" + btoa(`${title}/${formatNum(year,2)}/${formatNum(month,2)}/${undTitle}${isSunday?"_ntb":""}.${year}${formatNum(month,2)}${formatNum(day,2)}_${res}.${isSunday?"png":"gif"}`);
 };
-const regex = /([^\/]+)\/\d+\/\d+\/([^.]+)((?:_[^.]+)|)\.(\d{4})(\d{2})(\d{2})_(\d+)\.(.+)$/;
+const regex = /([^\/]+)\/\d+\/\d+\/([^.]+)\.(\d{4})(\d{2})(\d{2})_(\d+)\.(.+)$/;
 const isSunday = (year, month, day) => moment(`${year}-${month}-${day}`, "YYYY-MM-DD").format("d") == 0;
-const formatNum = (num, digits = 2) => num.toString().padStart(digits, "0")
+const formatNum = (num, digits = 2) => num.toString().padStart(digits, "0");
+app.get("/",(req, res) => res.send("This is under construction."));
 app.post("/getUrl", (req, res) => {
     const {
         base64,
@@ -24,10 +25,12 @@ app.post("/getUrl", (req, res) => {
         day
     } = req.body;
     const mom = moment(`${year}-${month}-${day}`,"YYYY-MM-DD");
-    const [_, name, escName, extension, sampleYear, sampleMonth, sampleDay, size, format] = Buffer(base64, "base64").toString().match(regex);
-    const undName = isSunday(sampleYear, sampleMonth, sampleDay) ? escName : escName + extension;
+    const [_, name, escName, sampleYear, sampleMonth, sampleDay, size, format] = Buffer(base64, "base64").toString().match(regex);
+    const undName = isSunday(sampleYear, sampleMonth, sampleDay) ? escName.split("_").slice(0,-1).join("_"):escName;
     //TODO Handle _ntb strips like dustin
     res.json({
+        undName,
+        escName,
         src: Buffer(isSunday(year, month, day) ? `${name}/${year}/${formatNum(month)}/${undName}_ntb.${year}${formatNum(month)}${formatNum(day)}_${size}.png` : `${name}/${year}/${formatNum(month)}/${undName}.${year}${formatNum(month)}${formatNum(day)}_${size}.gif`).toString("base64"),
         previous: mom.subtract(1, "day").format("YYYY-MM-DD"),
         next: mom.add(2, "day").format("YYYY-MM-DD"),
